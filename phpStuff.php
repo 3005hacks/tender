9c25a20c47de081b6601990de269bb630c8ca82c
jcbartlett25 <jcbart12595@gmail.com>
2015-03-08 08:51:13 -0400

    Ehhhhh

--- phpStuff.php
+++ phpStuff.php
@@ -1,53 +1,50 @@
 <?php
-  /* Send an SMS using Twilio. You can run this file 3 different ways:
-   *
-   * - Save it as sendnotifications.php and at the command line, run 
-   *        php sendnotifications.php
-   *
-   * - Upload it to a web host and load mywebhost.com/sendnotifications.php 
-   *   in a web browser.
-   * - Download a local server like WAMP, MAMP or XAMPP. Point the web root 
-   *   directory to the folder containing this file, and load 
-   *   localhost:8888/sendnotifications.php in a web browser.
-   */
-  // Include the PHP Twilio library. You need to download the library from 
-  // twilio.com/docs/libraries, and move it into the folder containing this 
-  // file.
-  require "twilio-php-master/Services/Twilio.php";
-  
-  // Set our AccountSid and AuthToken from twilio.com/user/account
-  $AccountSid = "ACae316fac018bbadcdfef6bce6eea7e40";
-  $AuthToken = "264541c0f23357675d11ea26bdcc707a";
-
-  // Instantiate a new Twilio Rest Client
-  $client = new Services_Twilio($AccountSid, $AuthToken);
-
-  /* Your Twilio Number or Outgoing Caller ID */
-  $from = '+16314173161';
-
-  // make an associative array of server admins. Feel free to change/add your 
-  // own phone number and name here.
-  $people = array(
-    "+19086357678" => "Amol",
-  );
-
-  // Iterate over all admins in the $people array. $to is the phone number, 
-  // $name is the user's name
-  foreach ($people as $to => $name) {
-    // Send a new outgoing SMS */
-    $body = "This shit better work";
-    $client->account->sms_messages->create($from, $to, $body);
-    echo "Sent message to $name";
-  }
-?>
-
-<?php
-// Get the PHP helper library from twilio.com/docs/php/install
-require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
+    /* Send an SMS using Twilio. You can run this file 3 different ways:
+     *
+     * - Save it as sendnotifications.php and at the command line, run 
+     *        php sendnotifications.php
+     *
+     * - Upload it to a web host and load mywebhost.com/sendnotifications.php 
+     *   in a web browser.
+     * - Download a local server like WAMP, MAMP or XAMPP. Point the web root 
+     *   directory to the folder containing this file, and load 
+     *   localhost:8888/sendnotifications.php in a web browser.
+     */
  
-// Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "ACae316fac018bbadcdfef6bce6eea7e40"; 
-$token = "{{ 264541c0f23357675d11ea26bdcc707a }}"; 
-$client = new Services_Twilio($sid, $token);
+    // Step 1: Download the Twilio-PHP library from twilio.com/docs/libraries, 
+    // and move it into the folder containing this file.
+    require "Services/Twilio.php";
  
-$client->account->messages->sendMessage("+16314173161", "+19086357678", "Jenny please?! I love you <3", "http://www.example.com/hearts.png");
+    // Step 2: set our AccountSid and AuthToken from www.twilio.com/user/account
+    $AccountSid = "AC2727ce935aa207c8c9963dad66a39973";
+    $AuthToken = "ACae316fac018bbadcdfef6bce6eea7e40";
+ 
+    // Step 3: instantiate a new Twilio Rest Client
+    $client = new Services_Twilio($AccountSid, $AuthToken);
+ 
+    // Step 4: make an array of people we know, to send them a message. 
+    // Feel free to change/add your own phone number and name here.
+    $people = array(
+        "+19086357678" => "Amol",
+    );
+ 
+    // Step 5: Loop over all our friends. $number is a phone number above, and 
+    // $name is the name next to it
+    foreach ($people as $number => $name) {
+ 
+        $sms = $client->account->messages->sendMessage(
+ 
+        // Step 6: Change the 'From' number below to be a valid Twilio number 
+        // that you've purchased, or the (deprecated) Sandbox number
+            "631-417-3161", 
+ 
+            // the number we are sending to - Any phone number
+            $number,
+ 
+            // the sms body
+            "Hey $name, Monkey Party at 6PM. Bring Bananas!"
+        );
+ 
+        // Display a confirmation message on the screen
+        echo "Sent message to $name";
+    }
---
+++ twilio-php-master/phpStuff.php
@@ -0,0 +1,53 @@
+<?php
+  /* Send an SMS using Twilio. You can run this file 3 different ways:
+   *
+   * - Save it as sendnotifications.php and at the command line, run 
+   *        php sendnotifications.php
+   *
+   * - Upload it to a web host and load mywebhost.com/sendnotifications.php 
+   *   in a web browser.
+   * - Download a local server like WAMP, MAMP or XAMPP. Point the web root 
+   *   directory to the folder containing this file, and load 
+   *   localhost:8888/sendnotifications.php in a web browser.
+   */
+  // Include the PHP Twilio library. You need to download the library from 
+  // twilio.com/docs/libraries, and move it into the folder containing this 
+  // file.
+  require "/Services/Twilio.php";
+  
+  // Set our AccountSid and AuthToken from twilio.com/user/account
+  $AccountSid = "ACae316fac018bbadcdfef6bce6eea7e40";
+  $AuthToken = "264541c0f23357675d11ea26bdcc707a";
+
+  // Instantiate a new Twilio Rest Client
+  $client = new Services_Twilio($AccountSid, $AuthToken);
+
+  /* Your Twilio Number or Outgoing Caller ID */
+  $from = '+16314173161';
+
+  // make an associative array of server admins. Feel free to change/add your 
+  // own phone number and name here.
+  $people = array(
+    "+19086357678" => "Amol",
+  );
+
+  // Iterate over all admins in the $people array. $to is the phone number, 
+  // $name is the user's name
+  foreach ($people as $to => $name) {
+    // Send a new outgoing SMS */
+    $body = "This shit better work";
+    $client->account->sms_messages->create($from, $to, $body);
+    echo "Sent message to $name";
+  }
+?>
+
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
+ 
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "ACae316fac018bbadcdfef6bce6eea7e40"; 
+$token = "{{ 264541c0f23357675d11ea26bdcc707a }}"; 
+$client = new Services_Twilio($sid, $token);
+ 
+$client->account->messages->sendMessage("+16314173161", "+19086357678", "Jenny please?! I love you <3", "http://www.example.com/hearts.png");
